555c8dee26d60ff512c370c695392fbffb183e16
EC2 Default User <ec2-user@ip-172-31-40-191.ec2.internal>
2022-02-12 11:48:28 +0000

    [add]flash機能の追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,21 +1,44 @@
 class BooksController < ApplicationController
-  def new
-  end
 
   def index
+    @books = Book.all
     @book = Book.new
   end
 
   def create
     book = Book.new(book_params)
-    book.save
-    redirect_to '/books/show'
+    if book.save
+      flash[:success] = "successfully"
+      redirect_to book_path(book.id)
+    else
+      flash.now[:danger] = "error"
+      render books_path
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    if book.update(book_params)
+      flash[:success] = "successfully"
+      redirect_to book_path(book.id)
+    else
+      flash.now[:danger] = "error"
+      render book__path(book.id)
+    end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  get 'books/new' => 'books#new'
-  post 'books' => 'books#create'
-  get 'books/show' => 'books#show'
-  get 'books/edit'
-  get root to: 'homes#top'
-  get '/books' => 'books#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get root to: 'homes#top'
+  resources :books
 end
